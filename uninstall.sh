36d6924c423ebf5ecd79a0d6356999b958069cb5
Victor Fung <twfccc@gmail.com>
2016-05-31 09:57:39 +0800

    Update uninstall.sh
    Correct comment

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # $author: twfcc@twitter
 # $PROG: uninstall.sh
-# $description: uninstall components installed by [gfw3proxy.sh|gfwtiny.sh]
+# $description: uninstall components which installed by [gfw3proxy.sh|gfwtiny.sh]
 # $Usage: $0
 # Public Domain use as your own risk.
 
